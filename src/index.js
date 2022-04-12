e319703aa61ad93ca763619c5dc562dbd41db5a1
defstiller <81063003+defstiller@users.noreply.github.com>
2022-04-12 04:59:44 -0500

    renamed element

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Error from "./Error"
 import About from "./AboutPage/About"
 import Cocktail from "./Cocktails/Cocktail"
 import Home from "./Home"
+import CocktailDetails from './Cocktails/CocktailDetails';
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
@@ -14,7 +15,7 @@ root.render(
     <Routes>
       <Route exact path="/" element={<><Navbar /><Home /></>} />
       <Route exact path="about" element={<><Navbar /><About /></>} />
-      <Route path='/person/:id' element={<Cocktail/>} />
+      <Route path='/product/:id' element={<CocktailDetails/>} />
       <Route path="*" element={<Error />}/>
     </Routes>
   </Router>
