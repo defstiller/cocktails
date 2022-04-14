ac30de47b80e4ab7c1f804d31ec1465b0db86d1d
defstiller <81063003+defstiller@users.noreply.github.com>
2022-04-14 06:19:37 -0500

    added props, getIngredients will find number of ingredients

--- src/Cocktails/CocktailDetails.js
+++ src/Cocktails/CocktailDetails.js
@@ -1,11 +1,53 @@
-import {Link, useParams} from "react-router-dom"
-import useFetchProducts from "./useFetchProducts"
+import {useState, useEffect, useCallback, useRef} from "react"
+import {useLocation} from "react-router-dom"
 function CocktailDetails() {
-    const {id} = useParams()
-    const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
-    const {products} = useFetchProducts()
-    return (
-        <p>{id}</p>
-    )
+    const location = useLocation()
+    const [ingredients, setIngredients] = useState([])
+    function getIngredients() {
+        const name = "strIngredient"
+        let index = 1
+        while(location.state.props[name+index]) {
+            const ingredientName = location.state.props[name+index]
+            setIngredients(steps => [...steps, ingredientName])
+            index += 1
+        }
+        return index
+    }
+    useEffect(() => {
+        getIngredients()
+    },[])
+
+    const {
+        strAlcoholic : isAlcoholic, 
+        strCategory : category, 
+        strDrink : name, 
+        strDrinkThumb : image, 
+        strGlass: glassType,
+        strInstructions : instructions,
+        strIBA : special,
+        strTags : tags,
+        strVideo : video,
+        strDrinkAlternate : alternate,
+        strCreativeCommonsConfirmed : creativeCommons,
+        strImageAttribution : imageAttribution,
+     } = location.state.props 
+     return  <>
+        <figure>
+            <img src={image} alt={name} />
+            {imageAttribution && <figcaption>{imageAttribution}</figcaption>}
+        </figure>
+        {video && <video controls autoplay muted src={video}>
+            Your browser does not support the video tag.
+        </video> }
+        <p>{name}</p>
+        {ingredients.map(step => <p key={Math.random() * 300}>{step}</p>)}
+        <p>{isAlcoholic}</p>
+        <p>{category}</p>
+        <p>{instructions}</p>
+    </>
+
+        
+
+    
 }
 export default CocktailDetails
